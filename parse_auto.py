73284c62c4f5ef822deb4c1cb9707a27c997fa8d
David Manthey <david.manthey@kitware.com>
2025-03-07 22:16:41 -0500

    Update tracks

--- parse_auto.py
+++ parse_auto.py
@@ -11,7 +11,7 @@ for f in sys.argv[1:]:
         source += open(f).readlines()
 consts = {}
 actions = {}
-pos = {}
+posdict = {}
 curaction = None
 for line in source:  # noqa
     constval = re.match(
@@ -29,23 +29,23 @@ for line in source:  # noqa
         line)
     if posval:
         dp, where, x, y, h = posval.groups()
-        pos.setdefault(dp, {})
+        posdict.setdefault(dp, {})
         vals = [x, y, h]
         for idx, val in enumerate(vals):
             for c in consts:
                 if c in val:
                     val = val.replace(c, str(consts[c]))
             vals[idx] = eval(val)
-        pos[dp][where] = vals
+        posdict[dp][where] = vals
     posval = re.match(
         r'^\s*drivePositions(\w+)\.put\s*\(\s*"([^"]+)"\s*,\s*robot\.pointTo\(\s*([-\d.]+)\s*,\s*([-\d.]+)\s*,\s*([-\d.]+)\s*,\s*([-\d.]+)\s*\)\s*\)',  # noqa
         line)
     if posval:
         dp, where, x, y, x2, y2 = posval.groups()
-        pos.setdefault(dp, {})
+        posdict.setdefault(dp, {})
         vals = [float(x), float(y),
                 math.atan2(float(y2) - float(y), float(x2) - float(x)) * 180 / math.pi]
-        pos[dp][where] = vals
+        posdict[dp][where] = vals
     actionval = re.match(
         r'^\s*public\s+void\s+action(\w+)\s*\(\)\s*{',
         line)
@@ -62,21 +62,24 @@ for line in source:  # noqa
         r'^\s*driveTo\.setTargetPosition\(\s*drivePositions.get\(\s*"([^"]+)"\s*\)\s*(|,\s*([^,)]+)\s*)(|,\s*([^,)]+)\s*)\);',  # noqa
         line)
     if driveval and curaction:
-        actions[curaction]['position'] = driveval.groups()[0]
+        pos = {}
+        actions[curaction].setdefault('positions', [])
+        actions[curaction]['positions'].append(pos)
+        pos['position'] = driveval.groups()[0]
         val = driveval.groups()[2]
         val2 = driveval.groups()[4]
         if val is None and val2 is not None:
             val, val2 = val2, val
         if val2 is not None:
-            actions[curaction]['stop'] = val2 != 'false'
+            pos['stop'] = val2 != 'false'
         if val is not None:
             if val in {'true', 'false'}:
-                actions[curaction]['stop'] = val != 'false'
+                pos['stop'] = val != 'false'
             else:
                 for c in consts:
                     if c in val:
                         val = val.replace(c, str(consts[c]))
-                actions[curaction]['speed'] = eval(val)
+                pos['speed'] = eval(val)
     waitval = re.match(
         r'^\s*wait\s*=\s*getRuntime\(\)\s*\+\s*([^,)]+)\s*;',
         line)
@@ -88,32 +91,37 @@ for line in source:  # noqa
         actions[curaction]['wait'] = actions[curaction].setdefault('wait', 0) + float(val)
 
 
-actions['Start']['position'] = 'start'
+actions['Start'].setdefault('positions', [{'position': 'start'}])
 pprint.pprint(consts)
-pprint.pprint(pos)
+pprint.pprint(posdict)
 pprint.pprint(actions)
 
 
-def track_sequence(name, posname, action, sequences, seq=None):
+def track_sequence(name, posname, action, sequences, posnum=0, seq=None):
     if action not in actions:
         return
     if 'next' not in actions[action]:
         return
-    if 'position' in actions[action] and actions[action]['position'] not in pos[posname]:
+    pos = None
+    if 'positions' in actions[action]:
+        if posnum >= len(actions[action]['positions']):
+            return
+        pos = actions[action]['positions'][posnum]
+    if pos and pos['position'] not in posdict[posname]:
         return
-    if 'position' in actions[action]:
+    if pos:
         if seq is None:
             seq = []
         else:
             seq = seq[:]
-        seq.append([actions[action]['position']])
-        seq[-1].extend(pos[posname][actions[action]['position']])
-        if 'speed' in actions[action]:
-            seq[-1].append(actions[action]['speed'])
+        seq.append([pos['position']])
+        seq[-1].extend(posdict[posname][pos['position']])
+        if 'speed' in pos:
+            seq[-1].append(pos['speed'])
         else:
             seq[-1].append(None)
-        if 'stop' in actions[action]:
-            seq[-1].append(actions[action]['stop'])
+        if 'stop' in pos:
+            seq[-1].append(pos['stop'])
         else:
             seq[-1].append(None)
     if 'wait' in actions[action] and seq is not None and len(seq):
@@ -126,13 +134,22 @@ def track_sequence(name, posname, action, sequences, seq=None):
         if nextact == 'done':
             sequences[name] = seq
         elif len(actions[action]['next']) == 1:
-            track_sequence(name, posname, nextact, sequences, seq)
+            if nextact not in actions or 'positions' not in actions[nextact] or len(actions[nextact]['positions']) == 1:
+
+                track_sequence(name, posname, nextact, sequences, 0, seq)
+            else:
+                for posidx in range(len(actions[nextact]['positions'])):
+                    track_sequence(name + str(posidx), posname, nextact, sequences, posidx, seq)
         else:
-            track_sequence(name + nextact, posname, nextact, sequences, seq)
+            if nextact not in actions or 'positions' not in actions[nextact] or len(actions[nextact]['positions']) == 1:
+                track_sequence(name + nextact, posname, nextact, sequences, 0, seq)
+            else:
+                for posidx in range(len(actions[nextact]['positions'])):
+                    track_sequence(name + nextact + str(posidx), posname, nextact, sequences, posidx, seq)
 
 
 sequences = {}
-for posname in pos:
+for posname in posdict:
     track_sequence(posname, posname, 'Start', sequences)
 pprint.pprint(sequences)
 
