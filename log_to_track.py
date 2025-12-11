d7c76051e7218857d12ac11b584efdee631d50d1
manthey <manthey@users.noreply.github.com>
2025-12-11 14:23:37 +0000

    deploy: 3fd4b2f4c7ebf64f002856542a1a783e475daaeb

--- log_to_track.py
+++ log_to_track.py
@@ -1,10 +1,11 @@
 import argparse
 import csv
+import math
 import os
 import sys
 
 
-def logs_to_tracks(logdir, output, runs):
+def logs_to_tracks(logdir, output, runs, realign=False):
     out = ["""Setting,fixedTimes,true
 Setting,stopTime,0
 Setting,showText,false
@@ -12,7 +13,9 @@ Setting,showArrows,false
 Robot,0,https://manthey.github.io/ftc-tracks/decodebot33dpi.png,8.25,8.75,8.25,8.75
 Field,https://manthey.github.io/ftc-tracks/decode.png,72,72,72,72"""]
     tracks = {}
-    for file in os.listdir(logdir):
+    lastname, lastnum = None, None
+    lastpose = None
+    for file in sorted(os.listdir(logdir), reverse=True):
         if not file.endswith('csv'):
             continue
         try:
@@ -21,6 +24,10 @@ Field,https://manthey.github.io/ftc-tracks/decode.png,72,72,72,72"""]
         except Exception:
             continue
         track = []
+        basepose = None
+        if lastpose is not None and not name.startswith('Auto') and realign:
+            basepose = tuple(list(lastpose))
+        startpose = None
         t0 = None
         with open(os.path.join(logdir, file), 'r', newline='', encoding='utf-8') as fptr:
             reader = csv.reader(fptr)
@@ -30,7 +37,18 @@ Field,https://manthey.github.io/ftc-tracks/decode.png,72,72,72,72"""]
                     if t0 is None:
                         t0 = t
                     x, y, h = (float(v.strip('"')) for v in line[4].strip('"').split())
+                    if startpose is None and basepose is not None:
+                        startpose = (x, y, h, (basepose[2] - h) * math.pi / 180)
+                    if basepose is not None:
+                        x1 = x - startpose[0]
+                        y1 = y - startpose[1]
+                        h1 = h - startpose[2]
+                        x2 = x1 * math.cos(startpose[3]) - y1 * math.sin(startpose[3])
+                        y2 = x1 * math.sin(startpose[3]) + y1 * math.cos(startpose[3])
+                        x, y, h = round(x2 + basepose[0], 2), round(y2 + basepose[1], 2), round(h1 + basepose[2], 2)
                     track.append((t - t0, x, y, h))
+                    lastpose = (x, y, h)
+        lastname, lastnum = name.split('-')[0], number
         if len(track) < 2:
             continue
         tracks[number] = {'name': name, 'track': track, 'number': number}
@@ -38,7 +56,7 @@ Field,https://manthey.github.io/ftc-tracks/decode.png,72,72,72,72"""]
         name = tracks[number]['name']
         track = tracks[number]['track']
         skip = runs and number not in runs
-        sys.stderr.write(f'{number} {track[-1][0]} {name} {"- skipped" if skip else ""}\n')
+        sys.stderr.write(f'{number:3d} {track[-1][0]:7.3f} {name} {"- skipped" if skip else ""}\n')
         if skip:
             continue
         out.append(f'Path,{name}')
@@ -135,6 +153,9 @@ if __name__ == '__main__':
         description='Convert a diretcory of telemetry logs into a track record')
     parser.add_argument('logdir', help='Log directory')
     parser.add_argument('-o' ,'--output', help='Track file path')
+    parser.add_argument(
+        '--realign', action='store_true',
+        help='Realign teleop paths to previous path')
     parser.add_argument('-x' ,'--excel', help='Excel file output path')
     parser.add_argument(
         '-c' ,'--csv',
@@ -143,5 +164,5 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--runs', help='Comma-separated list of run numbers to process.')
     opts = parser.parse_args()
     runs = [int(r) for r in opts.runs.split(',')] if opts.runs is not None else None
-    logs_to_tracks(opts.logdir, opts.output, runs)
+    logs_to_tracks(opts.logdir, opts.output, runs, opts.realign)
     logs_to_excel(opts.logdir, opts.excel, opts.csv, runs)
